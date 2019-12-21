04fa949682c954ad83ec5d790a6b14a4c2a9f104
Daniel Del Core <daniel.delcore.92@gmail.com>
2019-12-21 18:49:54 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+<p align="center">
+  <img width="580" src="assets/logo.png" alt="Script Palette">
+</p>
+
+# ScriptPal 🤘
+
+A simple npm script palette for lazy people (like me), who want a quick way to look through and pick npm scripts!
+
+- keyboard navigation
+- autocompletion
+- fussy finding
+
+## Install ⬇️
+
+Install globally
+
+```bash
+npm install -g scriptpal
+```
+
+## Get started 🏁
+
+```bash
+scriptpal
+```
+
+Usage with npx
+
+```bash
+npx scriptpal
+```
---
+++ bin/index.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+"use strict";
+
+const { prompt, AutoComplete } = require("enquirer");
+const clipboardy = require("clipboardy");
+const meow = require("meow");
+
+const welcome = require("./welcome");
+const { getPackageJson, hasFile } = require("./file-manager");
+
+async function main(input, flags) {
+  console.log(input, flags);
+
+  if (!flags.nowelcome) {
+    welcome();
+  }
+
+  try {
+    const packageJson = await getPackageJson("package.json");
+
+    const prompt = new AutoComplete({
+      name: "flavor",
+      message: "Which script would you like to run? 🤷‍♂️",
+      limit: 18,
+      choices: Object.keys(packageJson.scripts)
+    });
+
+    const script = await prompt.run();
+    const packageManager = hasFile("yarn.lock") ? "yarn" : "npm run";
+    const command = `${packageManager} ${script}`;
+
+    await clipboardy.write(command);
+
+    console.log("Copied to clipboard 👉 📋");
+  } catch (error) {
+    if (error) {
+      console.error(error, "🙅‍♂️");
+      return 1;
+    }
+
+    return 0;
+  }
+}
+
+const cli = meow(
+  `
+	Usage
+	  $ scriptpal
+
+	Options
+	  --nowelcome, -n  Omit welcome message
+	  --help  Help me
+	  --version, -v  Version number
+
+	Examples
+	  $ scriptpal --nowelcome
+	  $ npx scriptpal
+`,
+  {
+    flags: {
+      nowelcome: {
+        type: "boolean",
+        alias: "n"
+      }
+    }
+  }
+);
+
+main(cli.input[0], cli.flags);
